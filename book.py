4ead1556731743736149a3b97901804dc54ed514
Horace <107565811+1024-Java@users.noreply.github.com>
2024-04-21 15:35:45 +0800

    Update book.py

--- book.py
+++ book.py
@@ -6,6 +6,7 @@ with open(file,encoding='gbk') as f:
 	# 寻找字符串并返回索引
 	index = book.find("python")
 	# 创建一个新的文本文件来保存指定内容
+	# a.txt | b.txt |  c.txt......
 	with open("a.txt","w") as b:
 		b.write(book[:index])
 		# 更新·文件名 || 删除了前段部分，保留后段
